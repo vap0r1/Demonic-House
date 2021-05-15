9c6cf1f492ff224cc5977e6eccd600f281d17233
vap0r1 <kardargo@gmail.com>
2021-05-14 17:13:29 -0700

    fix4

---
+++ Story/dh.js
@@ -0,0 +1,10 @@
+Config.passages.nobr = true;
+
+
+
+
+Config.saves.autosave = ["bookmark", "autosave"];
+Config.saves.id = "DemonHouse";
+Config.passages.descriptions = {
+	"Intro_2" : "description"
+};
